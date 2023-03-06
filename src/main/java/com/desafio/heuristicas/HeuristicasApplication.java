7b2393806b808aa168a9141b3c5a628aa6846b77
Marco Tulio <marcotfrancosilva@gmail.com>
2023-03-06 19:28:18 -0300

    criando o projeto

---
+++ src/main/java/com/desafio/heuristicas/HeuristicasApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.heuristicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeuristicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeuristicasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/heuristicas/HeuristicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.heuristicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeuristicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
